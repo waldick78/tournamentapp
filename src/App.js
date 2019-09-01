efd2e8a9bda793ad82d676b4fde066955d2876ba
waldick11 <52767773+waldick78@users.noreply.github.com>
2019-08-31 18:39:48 -0700

    Third commit
    Added a function to make disappear name from Players Array Box, after clicking Add Player Button.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import React from 'react';
 
+import React from 'react';
+
 class App extends React.Component{
     constructor(){
         super();
         this.state = {
-          playerArr:['Herc','Daniel','Megan','Vanessa','Kat','Jordan','Ben','Corey','Darnel','Greg','Mike','Ange'],
-          winnerArr:['Ben','Jordan'],
-
+          playerArr:[""],
+          winnerArr:["dani"],
+          player1: "",
+          player2: "",
+          value:""
         }
     }
 
-    getPlayer =()=>{
+getPlayer =()=>{
       let arr = this.state.playerArr;
       let player = arr.pop();
       this.setState({playerArr:arr});
-      console.log(arr);
       return player;
     }
 
-componentDidMount(){
-let popPlayer =  this.getPlayer()
-  console.log(popPlayer);
+getPlayers=()=>{
+  let popPlayer1 =  this.getPlayer()
+  let popPlayer2 =  this.getPlayer()
+  this.setState({
+    player1: popPlayer1,
+    player2: popPlayer2
+  })
+  }
 
+  handleChange=(event)=>{
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit=(event)=>{
+    this.setState({playerArr: [...this.state.playerArr, this.state.value], value:""})
+    event.preventDefault();
+  }
+
+  playGame=()=>{
+    this.getPlayers();
+  }
+
+  render(){
+
+    return(
+      <div>
+      <div>
+        {this.state.player1} VS {this.state.player2}
+      </div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+            <input placeholder="Your name here" value={this.state.value} onChange={this.handleChange}/>
+            <input type="submit" value="Add Player" />
+        </form>
+        <button onClick={this.playGame}>Play Game</button>
+      </div>
+      </div>
+     )
+  }
 }
-
-    render(){
-      // put js herege
-
-
-        return(
-            <div>
-              {this.state.playerArr}
-            </div>
-
-        )
-    }
-}
-
-
 
 export default App;
