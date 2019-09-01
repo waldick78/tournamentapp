5f26e76b7c41d617843d645da63802cdae30a9c5
waldick11 <52767773+waldick78@users.noreply.github.com>
2019-09-01 12:36:57 -0700

    Fourth commit
    Added more functionality to player1 and player2 buttons.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,80 @@
 import React from 'react';
 
-import React from 'react';
-
 class App extends React.Component{
     constructor(){
         super();
         this.state = {
-          playerArr:[""],
-          winnerArr:["dani"],
-          player1: "",
-          player2: "",
-          value:""
+          matches: [
+            { player1: "player1", player2: "player2", played: false },
+            { player1: "player3", player2: "player4", played: false },
+          ],
+          player1: '',
+          player2: '',
+          value:"",
+          winner1: null
         }
     }
 
-getPlayer =()=>{
-      let arr = this.state.playerArr;
-      let player = arr.pop();
-      this.setState({playerArr:arr});
-      return player;
-    }
-
-getPlayers=()=>{
-  let popPlayer1 =  this.getPlayer()
-  let popPlayer2 =  this.getPlayer()
-  this.setState({
-    player1: popPlayer1,
-    player2: popPlayer2
-  })
+  handleChangePlayer1=(event)=>{
+    this.setState({player1: event.target.value})
   }
 
-  handleChange=(event)=>{
-    this.setState({value: event.target.value})
+  handleChangePlayer2=(event)=>{
+    this.setState({player2: event.target.value})
   }
 
-  handleSubmit=(event)=>{
-    this.setState({playerArr: [...this.state.playerArr, this.state.value], value:""})
+  addGame=(event)=>{
+    // this.setState({playerArr: [...this.state.playerArr, this.state.value], value:""})
+    this.setState({
+      matches: [
+        ...this.state.matches,
+        { player1: this.state.player1, player2: this.state.player2, played: false }
+      ],
+      player1: '',
+      player2: ''
+    })
+
     event.preventDefault();
   }
 
-  playGame=()=>{
-    this.getPlayers();
+  addMatch = (player1, player2) => {
+    this.setState({
+      matches: [
+        ...this.state.matches,
+        { player1, player2, played: false }
+      ]
+    })
+  }
+
+  winner = (player) => {
+    if(this.state.winner1 === null) {
+      this.setState({ winner1: player })
+    } else {
+      this.addMatch(this.state.winner1, player)
+      this.setState({ winner1: null })
+    }
   }
 
   render(){
-
     return(
       <div>
-      <div>
-        {this.state.player1} VS {this.state.player2}
-      </div>
-      <div>
-        <form onSubmit={this.handleSubmit}>
-            <input placeholder="Your name here" value={this.state.value} onChange={this.handleChange}/>
-            <input type="submit" value="Add Player" />
-        </form>
-        <button onClick={this.playGame}>Play Game</button>
-      </div>
+          <form onSubmit={this.addGame}>
+              <input placeholder="Player 1" onChange={this.handleChangePlayer1} value={this.state.player1} />
+              <input placeholder="Player 2" onChange={this.handleChangePlayer2} value={this.state.player2} />
+              <input type="submit" value="Add Match" />
+          </form>
+          <ol>
+            {this.state.matches.map(match => (
+              <li key={match.player1+match.player2}>
+                {match.player1} <button disabled={match.played === true} onClick={() => this.winner(match.player1) }>Winner!</button>
+                VS
+                {match.player2} <button onClick={() => this.winner(match.player2) }>Winner!</button>
+              </li>
+            ))}
+          </ol>
+          <div>
+            Winner: {this.state.winner1}
+          </div>
       </div>
      )
   }
