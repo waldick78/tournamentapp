287ffa2ca631be44a9992fdc39e9900ea6b2f0a7
waldick11 <52767773+waldick78@users.noreply.github.com>
2019-09-02 13:41:32 -0700

    sixth commit
    Added CSS styling and media query

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import './App.css';
+
+const Winner = (props) => (
+  <div className="winner-section">
+    <img src="https://image.flaticon.com/icons/png/512/321/321773.png" width="50" height="50"  />
+    Winner: {props.winnerName}
+  </div>
+)
 
 class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-          // each match inside matches contain player1, player2 and a flag called played indicading if that match was played or not
-          matches: [
-            { player1: "Andre", player2: "Walmir", played: false },
-            { player1: "Thiago", player2: "Daniel", played: false },
-          ],
-          player1: '', // will hold the value inside the input of player1
-          player2: '', // will hold the value inside the input of player2
-          previousWinner: null // will hold the value of the winner of the previous match
-        }
-    }
+  constructor(){
+      super();
+      this.state = {
+        // each match inside matches, contains player1, player2 and a flag called played indicading if that match was played or not
+        matches: [],
+        player1: '', // will hold the value inside the input of player1
+        player2: '', // will hold the value inside the input of player2
+        previousWinner: null // The initial state is Null, kind of like "0" or "false". It will hold the value of the winner of the previous match
+      }
+  }
 
   handleChangePlayer1=(event)=>{
     // called when the input of player1 is changed
-    this.setState({player1: event.target.value})
+    this.setState({ player1: event.target.value })
   }
 
   handleChangePlayer2=(event)=>{
     // called when the input of player2 is changed
-    this.setState({player2: event.target.value})
+    this.setState({ player2: event.target.value })
   }
 
   onAddGame=(event)=>{
@@ -34,13 +39,18 @@ class App extends React.Component{
 
   // receives 2 players as parameters
   addMatch = (player1, player2) => {
-    const newMatch = { player1: player1, player2: player2, played: false }
-    this.setState({ matches: this.state.matches.concat(newMatch) })
+    const newMatch = {
+      player1: player1,
+      player2: player2,
+      played: false
+    }
+    const newMatches = this.state.matches.concat(newMatch)
+    this.setState({ matches: newMatches })
   }
 
   markMatchAsPlayed = (playedMatch) => {
     // iterate over the matches to find what match is the played one
-    // once it finds it, it sets its played attribute to true and retur
+    // once it finds it, it sets its played attribute to true and return
     this.state.matches.map(match => {
       if(match !== playedMatch) {
         // if the match we are iterating on is not equal to playedMatch
@@ -75,26 +85,27 @@ class App extends React.Component{
   render(){
     return(
       <div>
-          <form onSubmit={this.onAddGame}>
-              <input placeholder="Player 1" onChange={this.handleChangePlayer1} value={this.state.player1} />
-              <input placeholder="Player 2" onChange={this.handleChangePlayer2} value={this.state.player2} />
-              <input type="submit" value="Add Match" />
-          </form>
-          <ol>
-            {this.state.matches.map(match => (
-              <li key={match.player1+match.player2}>
-                <span>
-                  {match.player1}
-                  <button disabled={match.played === true} onClick={() => this.play(match, match.player1)}>Win!</button>
-                </span>
-                <span> VS </span>
-                <span>{match.player2} <button disabled={match.played === true} onClick={() => this.play(match, match.player2)}>Win!</button></span>
-              </li>
-            ))}
-          </ol>
-          <div>
-            Winner: {this.state.previousWinner}
-          </div>
+        <form onSubmit={this.onAddGame}>
+            <input className="player-input" placeholder="Player 1" onChange={this.handleChangePlayer1} value={this.state.player1} />
+            <input className="player-input" placeholder="Player 2" onChange={this.handleChangePlayer2} value={this.state.player2} />
+            <input className="player-input" type="submit" value="Add Match" />
+        </form>
+        <ol>
+          {this.state.matches.map(match => (
+            <li key={match.player1+match.player2}>
+              <span>
+                {match.player1}
+                <button
+                  disabled={match.played === true}
+                  onClick={() => this.play(match, match.player1)}>Win!</button>
+              </span>
+              <span>   VS   </span>
+              <span>{match.player2} <button disabled={match.played === true} onClick={() => this.play(match, match.player2)}>Win!</button></span>
+            </li>
+          ))}
+        </ol>
+
+        <Winner winnerName={this.state.previousWinner} />
       </div>
      )
   }
